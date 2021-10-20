6324e57d53b7cde0cb92adee7ae44512f44b662a
Jonathan Chiang <jc91765@gmail.com>
2021-10-19 20:48:16 -0700

    set up data file local storage obj JSON

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 /* exported data */
+var savedFacts = {
+  entries: []
+};
+
+window.addEventListener('beforeunload', function (event) {
+  var savedFactsJSON = JSON.stringify(savedFacts);
+  this.localStorage.setItem('saved-facts', savedFactsJSON);
+});
+
+var previousSaves = localStorage.getItem('saved-facts');
+if (previousSaves !== null) {
+  savedFacts = JSON.parse(previousSaves);
+}
